4fc593fee4018dc3c5510584cd48e2458ac27b39
Raphael <raphaelceccato@hotmail.com>
2023-06-20 11:24:50 -0300

    enable session in app

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const session = require('express-session');
+const MemoryStore = require('memorystore')(session);
+
 const app = express();
 const port = 3000;
 
 app.set("view engine", "ejs");
+app.use(session({
+    cookie: { maxAge: 86400000 },
+    store: new MemoryStore({
+      checkPeriod: 86400000 // prune expired entries every 24h
+    }),
+    resave: false,
+    secret: '4742338820204490440'
+}));
 
 
 app.get("/", (req, res) => {
