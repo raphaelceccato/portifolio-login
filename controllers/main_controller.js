3e6c4fb14f128f0e4ce2989bede2cae1dbdad686
Raphael <raphaelceccato@hotmail.com>
2024-02-24 07:48:51 -0300

    pass username to index page

--- controllers/main_controller.js
+++ controllers/main_controller.js
@@ -5,7 +5,7 @@ let router = express.Router();
 
 
 router.get("/", (req, res) => {
-    res.render("page", { page: "index" });
+    res.render("page", { page: "index", usuario: req.session.usuario });
 });
 
 
